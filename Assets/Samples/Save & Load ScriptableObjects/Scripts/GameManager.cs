55c385ec6f7850524f2e7127b1b0c82c0fabc72a
neogeek <neogeek0101@gmail.com>
2019-09-26 19:16:57 -0400

    Added Save & Load ScriptableObjects sample.

---
+++ Assets/Samples/Save & Load ScriptableObjects/Scripts/GameManager.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+namespace SaveLoadScriptableObjects
+{
+
+    public class GameManager : MonoBehaviour
+    {
+
+        [SerializeField]
+        private GameStateReference _gameStateReference;
+
+        private void OnEnable()
+        {
+
+            _gameStateReference.Load();
+
+        }
+
+        private void OnDisable()
+        {
+
+            _gameStateReference.SavePlayerData();
+            _gameStateReference.SaveScoreData();
+
+        }
+
+    }
+
+}
